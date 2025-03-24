a2c5b7e2586c2d4fbf9bd538c8d540020484f9e7
Veronika Kashlej <veronika.kashlei@mail.ru>
2025-03-24 22:23:26 +0300

    feat: 11 passing

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -117,8 +117,17 @@ function getPolynom(...arg) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const map = new Map();
+  return function f() {
+    const key = JSON.stringify(func);
+    if (map.has(key)) {
+      return map.get(key);
+    }
+    const result = func();
+    map.set(key, result);
+    return result;
+  };
 }
 
 /**
@@ -136,8 +145,18 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function f() {
+    let error;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (err) {
+        error = err;
+      }
+    }
+    return error;
+  };
 }
 
 /**
@@ -163,8 +182,14 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function f(...args) {
+    const argStr = `${args.map((arg) => JSON.stringify(arg)).join(',')}`;
+    logFunc(`${func.name}(${argStr}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${argStr}) ends`);
+    return result;
+  };
 }
 
 /**
@@ -180,8 +205,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function f(...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
@@ -201,8 +228,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let index = startFrom - 1;
+  return function f() {
+    index += 1;
+    return index;
+  };
 }
 
 module.exports = {
